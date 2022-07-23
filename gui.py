6cfc71f1907b96a454e5e52bfdb9f5a456a7bd6a
Bennett Chang <bennett.chang@mdland.com>
2022-07-23 16:03:45 -0400

    Added clear button

--- gui.py
+++ gui.py
@@ -1,6 +1,5 @@
-from tkinter import CENTER, TRUE
+from tkinter import TRUE
 import PySimpleGUI as sg
-import os.path
 from os import path
 import download_svg
 import pull_svg
@@ -11,6 +10,7 @@ file_list_column = [
     [
         sg.Text("Musescore URL", size=(15,1)),
         sg.In(size=(25, 1), enable_events=True, key="-URL-"),
+        sg.Button("Clear", size=(5,1), button_color=('white', ''), enable_events=TRUE, key='-CLEAR-'),
     ],
     [
         sg.Text("Download Path", size=(15,1)),
@@ -39,4 +39,8 @@ while True:
         else:
             download_svg.download_musescore_svg(values["-URL-"], values["-FOLDER-"], PATH)
             pull_svg.svgs_to_pdf(values["-FOLDER-"])
+            sg.Popup('Score downloaded!')
+    if event == "-CLEAR-":
+        window.find_element("-URL-").Update('')
+
 window.close()
